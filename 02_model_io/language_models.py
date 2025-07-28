78fa59b4a002256fd26aeea1367b3248060eb84e
Kazuki Minemura <kazuki.minemura@gmail.com>
2025-07-28 21:20:50 +0900

    adopted to langchain 1.0.0

--- 02_model_io/language_models.py
+++ 02_model_io/language_models.py
@@ -1,14 +1,18 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI # langchain==1.0.0 
+
 
 chat = ChatOpenAI(
     model="gpt-3.5-turbo",
 )
 
-result = chat(
-    [
-        HumanMessage(content="こんにちは！"),
-    ]
-)
+# 会話履歴を含めてinvokeする
+messages = [
+    HumanMessage(content="茶碗蒸しの作り方を教えてください"),
+    AIMessage(content="まず卵と出汁を混ぜて、具材と一緒に器に入れて蒸します。"),
+    HumanMessage(content="英語に翻訳して"),
+]
 
-print(result.content)  # Outputs: こんにちは！
+response = chat.invoke(messages)
+
+print(response.content)
