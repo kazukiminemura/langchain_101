7ab8119687d6bcd570a0a4251075cfd215e32c60
Kazuki Minemura <kazuki.minemura@gmail.com>
2025-07-28 21:50:08 +0900

    added

---
+++ 02_model_io/prompt_and_language_model.py
@@ -0,0 +1,24 @@
+from langchain import PromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI # langchain==1.0.0 
+
+
+chat = ChatOpenAI(
+    model="gpt-3.5-turbo",
+)
+
+# 会話履歴を含めてinvokeする
+prompt = PromptTemplate(
+    template="{product}はどこの会社が開発した製品ですか？",
+    input_variables=["product"]
+)
+
+result = chat.invoke(
+    [
+        HumanMessage(
+            content=prompt.format(product="iPhone")
+            ),
+    ]
+)
+
+print(result.content)
